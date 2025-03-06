0c88fcf0e8bb4c31986d6a73e7772d73eab811df
jtw011 <114012855+jtw011@users.noreply.github.com>
2025-03-06 15:56:27 -0700

    prophets

---
+++ lesson09/scripts/prophets.js
@@ -0,0 +1,31 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+const getProphetData = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.table(data.prophets); // Check data in console
+    displayProphets(data.prophets);
+};
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let portrait = document.createElement('img');
+
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '200');
+        portrait.setAttribute('height', '250');
+
+        card.appendChild(fullName);
+        card.appendChild(portrait);
+        cards.appendChild(card);
+    });
+};
+
+getProphetData();
